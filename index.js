5da50c6ff6bb5c6a41f49142defc284a4738328c
Sarfaraj Shah <sarfarajshah009@gmail.com>
2022-03-15 10:28:35 +0530

    upsh update

---
+++ index.js
@@ -0,0 +1,180 @@
+const path = require("path");
+const fs = require("fs");
+
+let newobject = {};
+
+fs.readdir(`${__dirname}/files`, "utf8", (error, data) => {
+  // console.log("error", error, data);
+  console.log(
+    `[finding directory]: ${error !== "null" ? "directory found...!" : error}`
+  );
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+    // fileName = element.split('.');
+    // console.log(path.basename(element, '.json'));
+    //   data.forEach((element) => {
+    fs.readFile(`${__dirname}/files/${element}`, "utf8", (error, data) => {
+      let json = JSON.parse(data);
+
+      newobject = {
+        name: `name ${path.basename(element, '.json')}`,
+        image: `image ${path.basename(element, '.json')}`,
+        descript: json.descript,
+        attributes: json.attributes,
+      };
+      console.log(
+        `[copying data]: ${error !== "null" ? "data copied...!" : error}`
+      );
+
+      fs.writeFileSync(
+        `${__dirname}/files/${element}`,
+        JSON.stringify(newobject)
+      );
+
+      console.log(
+        `[writing data]: ${error !== "null" ? "data written...!" : error}`
+      );
+    });
+
+    if(data.length === index){
+        break;
+    }
+    //   });
+  }
+});
+
+/**
+ *  resolve: [Function: resolve],
+  normalize: [Function: normalize],
+  isAbsolute: [Function: isAbsolute],
+  join: [Function: join],
+  relative: [Function: relative],
+  toNamespacedPath: [Function: toNamespacedPath],
+  dirname: [Function: dirname],
+  basename: [Function: basename],
+  extname: [Function: extname],
+  format: [Function: bound _format],
+  parse: [Function: parse],
+  sep: '/',
+  delimiter: ':',
+  win32: <ref *1> {
+    resolve: [Function: resolve],
+    normalize: [Function: normalize],
+    isAbsolute: [Function: isAbsolute],
+    join: [Function: join],
+    relative: [Function: relative],
+    toNamespacedPath: [Function: toNamespacedPath],
+    dirname: [Function: dirname],
+    basename: [Function: basename],
+    extname: [Function: extname],
+    format: [Function: bound _format],
+    parse: [Function: parse],
+    sep: '\\',
+    delimiter: ';',
+    win32: [Circular *1],
+    posix: [Circular *2],
+    _makeLong: [Function: toNamespacedPath]
+  },
+  posix: [Circular *2],
+  _makeLong: [Function: toNamespacedPath]
+}
+ */
+
+/**
+ * {
+  appendFile: [Function: appendFile],
+  appendFileSync: [Function: appendFileSync],
+  access: [Function: access],
+  accessSync: [Function: accessSync],
+  chown: [Function: chown],
+  chownSync: [Function: chownSync],
+  chmod: [Function: chmod],
+  chmodSync: [Function: chmodSync],
+  close: [Function: close],
+  closeSync: [Function: closeSync],
+  copyFile: [Function: copyFile],
+  copyFileSync: [Function: copyFileSync],
+  cp: [Function: cp],
+  cpSync: [Function: cpSync],
+  createReadStream: [Function: createReadStream],
+  createWriteStream: [Function: createWriteStream],
+  exists: [Function: exists],
+  existsSync: [Function: existsSync],
+  fchown: [Function: fchown],
+  fchownSync: [Function: fchownSync],
+  fchmod: [Function: fchmod],
+  fchmodSync: [Function: fchmodSync],
+  fdatasync: [Function: fdatasync],
+  fdatasyncSync: [Function: fdatasyncSync],
+  fstat: [Function: fstat],
+  fstatSync: [Function: fstatSync],
+  fsync: [Function: fsync],
+  fsyncSync: [Function: fsyncSync],
+  ftruncate: [Function: ftruncate],
+  ftruncateSync: [Function: ftruncateSync],
+  futimes: [Function: futimes],
+  futimesSync: [Function: futimesSync],
+  lchown: [Function: lchown],
+  lchownSync: [Function: lchownSync],
+  lchmod: undefined,
+  lchmodSync: undefined,
+  link: [Function: link],
+  linkSync: [Function: linkSync],
+  lstat: [Function: lstat],
+  lstatSync: [Function: lstatSync],
+  lutimes: [Function: lutimes],
+  lutimesSync: [Function: lutimesSync],
+  mkdir: [Function: mkdir],
+  mkdirSync: [Function: mkdirSync],
+  mkdtemp: [Function: mkdtemp],
+  mkdtempSync: [Function: mkdtempSync],
+  open: [Function: open],
+  openSync: [Function: openSync],
+  opendir: [Function: opendir],
+  opendirSync: [Function: opendirSync],
+  readdir: [Function: readdir],
+  readdirSync: [Function: readdirSync],
+  read: [Function: read],
+  readSync: [Function: readSync],
+  readv: [Function: readv],
+  readvSync: [Function: readvSync],
+  readFile: [Function: readFile],
+  readFileSync: [Function: readFileSync],
+  readlink: [Function: readlink],
+  readlinkSync: [Function: readlinkSync],
+  realpath: [Function: realpath] { native: [Function (anonymous)] },
+  realpathSync: [Function: realpathSync] { native: [Function (anonymous)] },
+  rename: [Function: rename],
+  renameSync: [Function: renameSync],
+  rm: [Function: rm],
+  rmSync: [Function: rmSync],
+  rmdir: [Function: rmdir],
+  rmdirSync: [Function: rmdirSync],
+  stat: [Function: stat],
+  statSync: [Function: statSync],
+  symlink: [Function: symlink],
+  symlinkSync: [Function: symlinkSync],
+  truncate: [Function: truncate],
+  truncateSync: [Function: truncateSync],
+  unwatchFile: [Function: unwatchFile],
+  unlink: [Function: unlink],
+  unlinkSync: [Function: unlinkSync],
+  utimes: [Function: utimes],
+  utimesSync: [Function: utimesSync],
+  watch: [Function: watch],
+  watchFile: [Function: watchFile],
+  writeFile: [Function: writeFile],
+  writeFileSync: [Function: writeFileSync],
+  write: [Function: write],
+  writeSync: [Function: writeSync],
+  writev: [Function: writev],
+  writevSync: [Function: writevSync],
+  Dir: [class Dir],
+  Dirent: [class Dirent],
+  Stats: [Function: Stats],
+  ReadStream: [Getter/Setter],
+  WriteStream: [Getter/Setter],
+  FileReadStream: [Getter/Setter],
+  FileWriteStream: [Getter/Setter],
+  _toUnixTimestamp: [Function: toUnixTimestamp],
+ */
