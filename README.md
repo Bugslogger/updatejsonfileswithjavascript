5a63b748ea09619975f0031b477f20fe380fa586
Sarfaraj Shah <sarfarajshah009@gmail.com>
2022-03-15 11:01:01 +0530

    upadtes index.js

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # updatejsonfileswithjavascript
 
 when this code is executed it will check for json files in files folder and update it. you can specify what feilds should be updated or to update whole file
+
+ Author: MrDark
+ This code updates content inside file
+ you can modify code according to you what you have to change \
+ in the file. and type of files can be updated using this code
+ 
+ you must give credit to it's owner (MrDark [Sarfaraj Shah])
+ 
+ using this code you can update particular property of json file
+ or you can replace whole json file's data.
+ 
+ you can run this code any os using vsCode editor or any editor you prefered
+ recommended Visual code editor (vscode)
+ 
+ 
+ you have to replace files named foler content with your content
+ if you face any issue contact me on discord.
+ 
+ username: Mr.Dark#3119
+ Discord Server: Discord Server [https://discord.gg/SEdTFcukbF] 
+ youtube: Bugslogger[https://www.youtube.com/channel/UCEyxVbzsG0oj2f3qoUCQ00g]
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+/**
+ * Author: MrDark
+ * This code updates content inside file
+ * you can modify code according to you what you have to change \
+ * in the file. and type of files can be updated using this code
+ * 
+ * you must give credit to it's owner (MrDark [Sarfaraj Shah])
+ * 
+ * using this code you can update particular property of json file
+ * or you can replace whole json file's data.
+ * 
+ * you can run this code any os using vsCode editor or any editor you prefered
+ * recommended Visual code editor (vscode)
+ * 
+ * 
+ * you have to replace files named foler content with your content
+ * if you face any issue contact me on discord.
+ * 
+ * username: Mr.Dark#3119
+ * Discord Server: https://discord.gg/SEdTFcukbF
+ * youtube: Bugslogger [https://www.youtube.com/channel/UCEyxVbzsG0oj2f3qoUCQ00g]
+ */
+
 const path = require("path");
 const fs = require("fs");
 
 let newobject = {};
 
 fs.readdir(`${__dirname}/files`, "utf8", (error, data) => {
-  // console.log("error", error, data);
   console.log(
     `[finding directory]: ${error !== "null" ? "directory found...!" : error}`
   );
   for (let index = 0; index < data.length; index++) {
     const element = data[index];
-    // fileName = element.split('.');
-    // console.log(path.basename(element, '.json'));
-    //   data.forEach((element) => {
     fs.readFile(`${__dirname}/files/${element}`, "utf8", (error, data) => {
       let json = JSON.parse(data);
 
@@ -39,142 +58,5 @@ fs.readdir(`${__dirname}/files`, "utf8", (error, data) => {
     if(data.length === index){
         break;
     }
-    //   });
   }
 });
-
-/**
- *  resolve: [Function: resolve],
-  normalize: [Function: normalize],
-  isAbsolute: [Function: isAbsolute],
-  join: [Function: join],
-  relative: [Function: relative],
-  toNamespacedPath: [Function: toNamespacedPath],
-  dirname: [Function: dirname],
-  basename: [Function: basename],
-  extname: [Function: extname],
-  format: [Function: bound _format],
-  parse: [Function: parse],
-  sep: '/',
-  delimiter: ':',
-  win32: <ref *1> {
-    resolve: [Function: resolve],
-    normalize: [Function: normalize],
-    isAbsolute: [Function: isAbsolute],
-    join: [Function: join],
-    relative: [Function: relative],
-    toNamespacedPath: [Function: toNamespacedPath],
-    dirname: [Function: dirname],
-    basename: [Function: basename],
-    extname: [Function: extname],
-    format: [Function: bound _format],
-    parse: [Function: parse],
-    sep: '\\',
-    delimiter: ';',
-    win32: [Circular *1],
-    posix: [Circular *2],
-    _makeLong: [Function: toNamespacedPath]
-  },
-  posix: [Circular *2],
-  _makeLong: [Function: toNamespacedPath]
-}
- */
-
-/**
- * {
-  appendFile: [Function: appendFile],
-  appendFileSync: [Function: appendFileSync],
-  access: [Function: access],
-  accessSync: [Function: accessSync],
-  chown: [Function: chown],
-  chownSync: [Function: chownSync],
-  chmod: [Function: chmod],
-  chmodSync: [Function: chmodSync],
-  close: [Function: close],
-  closeSync: [Function: closeSync],
-  copyFile: [Function: copyFile],
-  copyFileSync: [Function: copyFileSync],
-  cp: [Function: cp],
-  cpSync: [Function: cpSync],
-  createReadStream: [Function: createReadStream],
-  createWriteStream: [Function: createWriteStream],
-  exists: [Function: exists],
-  existsSync: [Function: existsSync],
-  fchown: [Function: fchown],
-  fchownSync: [Function: fchownSync],
-  fchmod: [Function: fchmod],
-  fchmodSync: [Function: fchmodSync],
-  fdatasync: [Function: fdatasync],
-  fdatasyncSync: [Function: fdatasyncSync],
-  fstat: [Function: fstat],
-  fstatSync: [Function: fstatSync],
-  fsync: [Function: fsync],
-  fsyncSync: [Function: fsyncSync],
-  ftruncate: [Function: ftruncate],
-  ftruncateSync: [Function: ftruncateSync],
-  futimes: [Function: futimes],
-  futimesSync: [Function: futimesSync],
-  lchown: [Function: lchown],
-  lchownSync: [Function: lchownSync],
-  lchmod: undefined,
-  lchmodSync: undefined,
-  link: [Function: link],
-  linkSync: [Function: linkSync],
-  lstat: [Function: lstat],
-  lstatSync: [Function: lstatSync],
-  lutimes: [Function: lutimes],
-  lutimesSync: [Function: lutimesSync],
-  mkdir: [Function: mkdir],
-  mkdirSync: [Function: mkdirSync],
-  mkdtemp: [Function: mkdtemp],
-  mkdtempSync: [Function: mkdtempSync],
-  open: [Function: open],
-  openSync: [Function: openSync],
-  opendir: [Function: opendir],
-  opendirSync: [Function: opendirSync],
-  readdir: [Function: readdir],
-  readdirSync: [Function: readdirSync],
-  read: [Function: read],
-  readSync: [Function: readSync],
-  readv: [Function: readv],
-  readvSync: [Function: readvSync],
-  readFile: [Function: readFile],
-  readFileSync: [Function: readFileSync],
-  readlink: [Function: readlink],
-  readlinkSync: [Function: readlinkSync],
-  realpath: [Function: realpath] { native: [Function (anonymous)] },
-  realpathSync: [Function: realpathSync] { native: [Function (anonymous)] },
-  rename: [Function: rename],
-  renameSync: [Function: renameSync],
-  rm: [Function: rm],
-  rmSync: [Function: rmSync],
-  rmdir: [Function: rmdir],
-  rmdirSync: [Function: rmdirSync],
-  stat: [Function: stat],
-  statSync: [Function: statSync],
-  symlink: [Function: symlink],
-  symlinkSync: [Function: symlinkSync],
-  truncate: [Function: truncate],
-  truncateSync: [Function: truncateSync],
-  unwatchFile: [Function: unwatchFile],
-  unlink: [Function: unlink],
-  unlinkSync: [Function: unlinkSync],
-  utimes: [Function: utimes],
-  utimesSync: [Function: utimesSync],
-  watch: [Function: watch],
-  watchFile: [Function: watchFile],
-  writeFile: [Function: writeFile],
-  writeFileSync: [Function: writeFileSync],
-  write: [Function: write],
-  writeSync: [Function: writeSync],
-  writev: [Function: writev],
-  writevSync: [Function: writevSync],
-  Dir: [class Dir],
-  Dirent: [class Dirent],
-  Stats: [Function: Stats],
-  ReadStream: [Getter/Setter],
-  WriteStream: [Getter/Setter],
-  FileReadStream: [Getter/Setter],
-  FileWriteStream: [Getter/Setter],
-  _toUnixTimestamp: [Function: toUnixTimestamp],
- */
